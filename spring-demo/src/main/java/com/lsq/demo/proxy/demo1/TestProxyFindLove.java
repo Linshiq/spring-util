31ac00f80c87bc9ec7e3ea811282925db7b8d18e
linshiq <284244633@qq.com>
2020-03-26 19:28:50 +0800

    测试提交

--- spring-demo/src/main/java/com/lsq/demo/proxy/demo1/TestProxyFindLove.java
+++ spring-demo/src/main/java/com/lsq/demo/proxy/demo1/TestProxyFindLove.java
@@ -4,7 +4,8 @@ public class TestProxyFindLove {
 
     public static void main(String[] args) {
         Person person = (Person) new MeiPo().getInstance(new ZhangSan());
-        // 实际上下面这个方法并不会实际执行
+        // 实际上下面这个方法并不会实际执行 只会按照代理方法的invoke的顺序去执行
+        // 也就是说 findlove 和 findlove2结果是一致的
         System.out.println(person.findLove());;
         System.out.println(person.findLove2());
     }
